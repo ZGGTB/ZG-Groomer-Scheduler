fa176276ad11f0e5697aeec1a5b3a2a26cd56400
Kevin <kevingrimes@outlook.com>
2025-03-03 21:23:20 -0500

    Changing Express Payload to 50MB

--- Server/server.js
+++ Server/server.js
@@ -11,7 +11,8 @@ const app = express();
 const PORT = 3001;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+
 
 // Logging middleware for debugging
 app.use((req, res, next) => {
