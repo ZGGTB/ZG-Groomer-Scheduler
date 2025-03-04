3a1721553005be08ca6b004748c975602e9cdf20
Kevin <kevingrimes@outlook.com>
2025-03-03 19:06:34 -0500

    Test DB Change

--- Server/server.js
+++ Server/server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// Secret key for JWT signing (store securely in production)
+// Secret key for JWT signing (store key securely in production) 
 const secretKey = 'your-secret-key';
 
 // Open (or create) the SQLite database file named ZG-Groomer-Scheduler.db in a data folder.
